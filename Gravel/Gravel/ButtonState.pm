9fcbb47fbea4f6814f6a8c07bdda5ff8b61bcf83
Ben Evans <benjamin.john.evans@gmail.com>
2003-05-15 10:35:13 +0000

    get this one in as well

---
+++ Gravel/Gravel/ButtonState.pm
@@ -0,0 +1,50 @@
+package Gravel::ButtonState;
+
+use strict;
+use warnings;
+
+use lib '../';
+
+use Gravel::Effect;
+use Gravel::Effect::Tween;
+
+use Data::Dumper qw/DumperX/;
+
+our $VERSION = '0.10';
+
+
+sub new {
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $cf = ref($_[0]);
+    my %conf;
+    
+    if ($cf eq 'HASH') {
+		%conf = %{$_[0]};
+    } else {
+		%conf = @_;
+    }
+
+	my $self = {};
+
+	# Set default for the menu flag
+	$self->{_menu} = 0;
+
+	$self->{_start} = $conf{start};
+    $self->{_end} = $conf{end};
+    $self->{_depth} = $conf{depth} || 1;
+
+    $self->{_tx} = $conf{tx};
+    $self->{_ty} = $conf{ty};
+
+	bless $self => $class;
+
+	return $self;
+}
+
+
+#
+
+
+1;
+
